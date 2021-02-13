9e05c907e6bf92cc8ec730528052930db0dddd9f
John Allison <john123allison@gmail.com>
2021-02-13 18:50:04 -0500

    start main game loop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.constants import QUIT
+from pygame.font import SysFont
 import pygame.locals
 from player import Player
 
@@ -30,6 +32,28 @@ def main():
     # instantiate player and platform
     player = Player()
     platform_test = Platform()
+
+    # Create sprite groups
+    # TODO make this not global lol
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player)
+    all_sprites.add(platform_test)
+
+    ### MAIN GAME LOOP ###
+    while True:
+        for event in pygame.event.get():
+            # Handle quitting the game
+            if event.type == QUIT:
+                pygame.quit()
+                SysFont.exit()
+
+        displaysurface.fill((0, 0, 0))
+
+        for entity in all_sprites:
+            displaysurface.blit(entity.surf, entity.rect)
+
+        pygame.display.update()
+        FramePerSec.tick(FPS)
 
 
 if __name__ == '__main__':
