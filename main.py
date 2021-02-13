a3b3354b8e9f831279da78669e9ce215b87de843
John Allison <john123allison@gmail.com>
2021-02-13 14:32:26 -0500

    initial setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import pygame.locals
+
+
+def main():
+    # Setup stuff
+    # Reference: https://coderslegacy.com/python/pygame-platformer-game-development/
+    pygame.init()
+    vec = pygame.math.Vector2  # 2 for two dimensional
+    HEIGHT = 450
+    WIDTH = 400
+    ACC = 0.5
+    FRIC = -0.12
+    FPS = 60
+    FramePerSec = pygame.time.Clock()
+    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("PyFight")
